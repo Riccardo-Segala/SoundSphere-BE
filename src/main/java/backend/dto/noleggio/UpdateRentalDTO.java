97e746b66050c64a8e1808ce101f48f3657aa91c
riccardo <ricky.segala@live.it>
2025-04-28 14:35:15 +0200

    Creati DTO Noleggio

---
+++ src/main/java/backend/dto/noleggio/UpdateRentalDTO.java
@@ -0,0 +1,18 @@
+package backend.dto.noleggio;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
+
+/**
+ * DTO for {@link backend.model.Noleggio}
+ */
+public record UpdateRentalDTO (
+        UUID id,
+        LocalDate dataInizio,
+        LocalDate dataScadenza,
+        LocalDate dataRestituzione,
+        LocalDate dataPagamento,
+        double totale
+) implements Serializable {
+}
