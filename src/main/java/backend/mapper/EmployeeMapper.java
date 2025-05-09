444c61acde06710b1980ebe7c8a96961b3b5570b
Miriam Raimondi <miriam.raimondi@edu.unife.it>
2025-05-09 14:21:25 +0200

    Entità Dipendente Mapper, Controller, Service

--- src/main/java/backend/mapper/EmployeeMapper.java
+++ src/main/java/backend/mapper/EmployeeMapper.java
@@ -1,41 +1,33 @@
 package backend.mapper;
 
-import backend.dto.dipendente.CreateEmplyeeDTO;
+import backend.dto.dipendente.CreateEmployeeDTO;
 import backend.dto.dipendente.ResponseEmployeeDTO;
 import backend.dto.dipendente.UpdateEmployeeDTO;
 import backend.model.Dipendente;
 import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
-public interface EmployeeMapper {
-    @Mapping(source = "filialeId", target = "filiale.id")
-    Dipendente toEntity(CreateEmplyeeDTO createEmplyeeDTO);
+public interface EmployeeMapper extends GenericMapper<Dipendente, CreateEmployeeDTO, UpdateEmployeeDTO, ResponseEmployeeDTO> {
 
+    @Override
+    @Mapping(source = "filialeId", target = "filiale.id")
+    Dipendente fromCreateDto(CreateEmployeeDTO createEmployeeDTO);
+
+    @Override
     @Mapping(source = "filiale.id", target = "filialeId")
-    CreateEmplyeeDTO toDto(Dipendente dipendente);
+    ResponseEmployeeDTO toDto(Dipendente dipendente);
 
+    @Override
+    @Mapping(source = "filialeId", target = "filiale.id")
+    Dipendente fromUpdateDto(UpdateEmployeeDTO updateEmployeeDTO);
+
+    @Override
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(source = "filialeId", target = "filiale.id")
-    Dipendente partialUpdate(CreateEmplyeeDTO createEmplyeeDTO, @MappingTarget Dipendente dipendente);
+    Dipendente partialUpdateFromCreate(CreateEmployeeDTO createEmployeeDTO, @MappingTarget Dipendente dipendente);
 
-    @Mapping(source = "filialeId", target = "filiale.id")
-    Dipendente toEntity(UpdateEmployeeDTO updateEmployeeDTO);
-
-    @Mapping(source = "filiale.id", target = "filialeId")
-    UpdateEmployeeDTO toDto1(Dipendente dipendente);
-
+    @Override
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(source = "filialeId", target = "filiale.id")
-    Dipendente partialUpdate(UpdateEmployeeDTO updateEmployeeDTO, @MappingTarget Dipendente dipendente);
-
-    @Mapping(source = "filialeId", target = "filiale.id")
-    @Mapping(source = "vantaggioId", target = "vantaggio.id")
-    Dipendente toEntity(ResponseEmployeeDTO responseEmployeeDTO);
-
-    @InheritInverseConfiguration(name = "toEntity")
-    ResponseEmployeeDTO toDto2(Dipendente dipendente);
-
-    @InheritConfiguration(name = "toEntity")
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    Dipendente partialUpdate(ResponseEmployeeDTO responseEmployeeDTO, @MappingTarget Dipendente dipendente);
+    Dipendente partialUpdateFromUpdate(UpdateEmployeeDTO updateEmployeeDTO, @MappingTarget Dipendente dipendente);
 }
