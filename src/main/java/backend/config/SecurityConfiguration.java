c07f477bb81ac4801088bf2267569da50a78c958
Jacopo Petrini <jacopo.petrini@edu.unife.it>
2025-08-18 17:30:47 +0200

    Risoluzione problema preflight token

--- src/main/java/backend/config/SecurityConfiguration.java
+++ src/main/java/backend/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package backend.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +26,7 @@ public class SecurityConfiguration {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/v3/api-docs/**",
