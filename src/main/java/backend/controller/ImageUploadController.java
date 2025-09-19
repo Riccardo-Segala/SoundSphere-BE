5ad6edef890be819def774f98fbaf8aef5b0127b
riccardo <ricky.segala@live.it>
2025-09-19 19:38:16 +0200

    Sistemato il controller delle immagini con map json

--- src/main/java/backend/controller/ImageUploadController.java
+++ src/main/java/backend/controller/ImageUploadController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping(path="/api/upload", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -17,19 +20,20 @@ public class ImageUploadController {
     private final ImageStorageService storageService;
     @PostMapping(path="/image",
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> uploadImage(@RequestParam("imageFile") MultipartFile file) {
+    public ResponseEntity<Map<String, String>> uploadImage(@RequestParam("imageFile") MultipartFile file) {
         try {
             // Delega la logica di salvataggio al servizio
             String publicPath = storageService.store(file);
 
+            Map<String, String> response = Collections.singletonMap("path", publicPath);
+
             // SUCCESSO: Restituisce 200 OK con la stringa del path nel corpo
-            return ResponseEntity.ok(publicPath);
+            return ResponseEntity.ok(response);
 
         } catch (RuntimeException e) {
 
-            return ResponseEntity
-                    .status(500)
-                    .body("Errore durante l'upload: " + e.getMessage());
+            Map<String, String> errorResponse = Collections.singletonMap("error", "Errore durante l'upload: " + e.getMessage());
+            return ResponseEntity.status(500).body(errorResponse);
         }
     }
 }
