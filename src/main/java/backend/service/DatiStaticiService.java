7fc37393e3d7d62d2a1daf9f16191d1137c179db
riccardo <ricky.segala@live.it>
2025-09-25 10:24:17 +0200

    creazione metodo per dati statici (spedizione)

--- src/main/java/backend/service/DatiStaticiService.java
+++ src/main/java/backend/service/DatiStaticiService.java
@@ -1,11 +1,13 @@
 package backend.service;
 
 import backend.dto.dati_statici.CreateOrUpdateStaticDataDTO;
+import backend.dto.dati_statici.DeliveryLimitDataDTO;
 import backend.dto.dati_statici.ResponseStaticDataDTO;
 import backend.exception.ResourceNotFoundException;
 import backend.mapper.StaticDataMapper;
 import backend.repository.DatiStaticiRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,12 @@ import java.util.UUID;
 public class DatiStaticiService {
     private final DatiStaticiRepository repository;
     private final StaticDataMapper mapper;
+
+    @Value("${app.static-data.delivery-cost}")
+    private String deliveryCost;
+
+    @Value("${app.static-data.free-delivery-threshold}")
+    private String freeDeliveryThreshold;
 
     public ResponseStaticDataDTO getStaticDataByName(String name) {
         return repository.findByNome(name)
@@ -56,5 +64,17 @@ public class DatiStaticiService {
 
         repository.delete(existingEntity);
         return null;
+    }
+
+    public DeliveryLimitDataDTO getDeliveryDataAndLimits() {
+        var deliveryCost = repository.findByNome(this.deliveryCost)
+                .map(mapper::toDto)
+                .orElseThrow(() -> new ResourceNotFoundException("Dati statici non trovati per COSTO_CONSEGNA"));
+
+        var freeDeliveryThreshold = repository.findByNome(this.freeDeliveryThreshold)
+                .map(mapper::toDto)
+                .orElseThrow(() -> new ResourceNotFoundException("Dati statici non trovati per SOGLIA_SPESA_CONSEGNA_GRATUITA"));
+
+        return new DeliveryLimitDataDTO(deliveryCost.valore(), freeDeliveryThreshold.valore());
     }
 }
